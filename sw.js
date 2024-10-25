c139cfdc394d8e0fbb658e16f59425574a0ce317
eer50 <githubpages1@outlook.com>
2024-10-25 04:33:40 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('age-calculator-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json'
+                // add other resources if needed
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
